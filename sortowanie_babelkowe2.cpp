3c51bd8fadba6a62f0bfa7e382d1343a50fc46db
Liwermor <dawidsochacki8@gmail.com>
2017-12-11 13:48:10 +0100

    Create sortowanie_babelkowe2.cpp

---
+++ sortowanie_babelkowe2.cpp
@@ -0,0 +1,34 @@
+#include<cstdlib>
+#include<iostream>
+#include<ctime>
+#include<algorithm>
+using namespace std; 
+int main()
+{
+	int t[20],k,pom;
+		srand(time(NULL));
+		//losowanie do tablicy
+		for(int i=0;i<20;i++){
+			t[i] = rand()%100;
+			cout<<t[i] << " ";
+		}cout<<endl;
+		//sortowanie
+		sort(t, t+20);
+/*		for(int i=19;i>0;i--){
+			for(int j=0;j<i;j++)
+			if(t[j]>t[j+1])
+				{pom=t[j+1];
+				t[j+1]=t[j];
+				t[j]=pom;
+				}
+	**/		
+		
+		//wysw wyniku
+		for(int i=0;i<20;i++)
+			cout<<t[i]<<" ";
+			cout<<endl;
+			return 0;
+		
+	
+}
+//sortowanie babelkowe elo. :-)
